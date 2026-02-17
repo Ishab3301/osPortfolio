402daf1ce77ba6677cc599d81d33a9d80b50784b
Ishab Dhakal <ishab3301@gmail.com>
2026-02-17 12:05:37 +0545

    Improve auto push script

--- auto-push.bat
+++ auto-push.bat
@@ -1,19 +1,9 @@
 @echo off
 cd /d C:\xampp\htdocs\osPortfolio
 
-git status --porcelain > changes.txt
-
-if %errorlevel% neq 0 (
-  exit /b
-)
-
-if not exist changes.txt exit /b
-for %%A in (changes.txt) do (
-  if %%~zA==0 exit /b
-)
+git diff --quiet && git diff --cached --quiet
+if %errorlevel%==0 exit /b
 
 git add .
 git commit -m "Daily auto update %date% %time%"
 git push origin main
-
-del changes.txt
