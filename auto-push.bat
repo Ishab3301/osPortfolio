c1990271b06056713e3f25d1cacae48faaab5e40
Ishab Dhakal <ishab3301@gmail.com>
2026-02-17 12:06:32 +0545

    Use smart daily auto push script

--- auto-push.bat
+++ auto-push.bat
@@ -1,8 +1,11 @@
 @echo off
 cd /d C:\xampp\htdocs\osPortfolio
 
-git diff --quiet && git diff --cached --quiet
-if %errorlevel%==0 exit /b
+git diff --quiet
+if %errorlevel%==0 (
+  git diff --cached --quiet
+  if %errorlevel%==0 exit /b
+)
 
 git add .
 git commit -m "Daily auto update %date% %time%"
